0e6ee3940c8cd5e541b25f7ca48ccdfd4152dc6a
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-01-27 23:04:50 -0600

    main.js and locations added

---
+++ init-locations.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        Name: '68 Block',
+        Latitude: 40.360465,
+        Longitude: -94.887566
+    },
+    {
+        Name: '56 block',
+        Latitude: 40.360970,
+        Longitude: -94.888242
+    }
+
+];
---
+++ scripts/main.js
@@ -0,0 +1,83 @@
+import locationsArray from '../init-locations.js';
+
+// for location element we assigned location id.
+let locationElement = document.getElementById("location");
+
+window.addEventListener('load', main);
+locationElement.addEventListener('click', locationHandler);
+locationElement.addEventListener('touch', locationHandler);
+
+function main() {
+    console.log('Page is fully loaded');
+}
+
+// initializing the current position lat and lon and error to true
+let currentlat;
+let currentlon;
+let error = true;
+
+// collects current location
+async function getLocation() {
+    return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    }).then(position => {
+        return position;
+    });
+}
+
+// getLocation function to itearte through the list of locations
+
+async function locationHandler() {
+    let locText = await getLocation();
+    currentlat = locText.coords.latitude;
+    document.getElementById("device-lat").innerHTML = "This is about device-lat: " + currentlat.toFixed(6);
+    currentlon = locText.coords.longitude;
+    document.getElementById("device-long").innerHTML = "This is about device-long: " + currentlon.toFixed(6);
+
+    locationsArray.forEach(function (value) {
+        if (isInside(value.Latitude, value.Longitude)) {
+            document.getElementById("locationAnswer").innerHTML = value.Name;
+            error = false;
+        }
+    });
+
+    if(error) {
+        document.getElementById("error-message").innerHTML = "You're not 30 meters nearer to any place.";
+    } else {
+        document.getElementById("error-message").innerHTML = "";
+    }
+}
+
+
+// distance between the locations
+
+function distanceBetweenLocations(questLat, questLon) {
+    const R = 6371e3;
+    const φ1 = currentlat * Math.PI / 180;
+    const φ2 = questLat * Math.PI / 180;
+    const Δφ = (questLat - currentlat) * Math.PI / 180;
+    const Δλ = (questLon - currentlon) * Math.PI / 180;
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    const d = R * c;
+    return d; // in metres
+}
+
+
+//Calculates distance and checks if within the range of 30 meters
+function isInside(questLat, questLon) {
+    let distance = distanceBetweenLocations(questLat, questLon);
+    console.log("distance: " + distance);
+    if (distance < 30) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
