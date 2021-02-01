6409dae83ea21d543441da92a91f67a2650051a2
Vikas2004 <S538336@NWMISSOURI.EDU>
2021-02-01 09:32:23 -0600

    locations add

--- init-locations.js
+++ init-locations.js
@@ -8,6 +8,22 @@ export default [
         Name: 'Admin Block',
         Latitude: 40.35341672746787, 
         Longitude: -94.88354434774271
+    },
+    {
+        Name: 'Foster Fitness Center',
+        Latitude: 40.35069401327249, 
+        Longitude: -94.88416885851595
+    },
+    {
+        Name: 'Bearcat Book Store',
+        Latitude: 40.3518223470855, 
+        Longitude:-94.88336419581596
+    },
+    {
+        Name: 'Bearcat Stadiium',
+        Latitude: 40.35073819617418, 
+        Longitude: -94.88584822342064
     }
+
 
 ];
